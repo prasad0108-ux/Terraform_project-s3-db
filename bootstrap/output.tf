7a44cdc59f3724dded2403b0135c2a911f684cd0
prasad0108-ux <suryavanshipr30@gmail.com>
2025-10-05 10:05:04 +0530

    Update output.tf

--- bootstrap/output.tf
+++ bootstrap/output.tf
@@ -1,5 +1,6 @@
-cd terraform-aws-project/bootstrap
-
-# initialize and create the backend resources
-terraform init
-terraform apply -var="state_bucket_name=your-unique-tfstate-bucket-12345" -auto-approve
+output "bucket" {
+  value = aws_s3_bucket.tf_state.bucket
+}
+output "dynamodb_table" {
+  value = aws_dynamodb_table.tf_locks.name
+}
