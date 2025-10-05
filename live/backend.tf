114ac7d4e5ae609bc6110a26dca47d1c2afd447b
prasad0108-ux <suryavanshipr30@gmail.com>
2025-10-05 14:09:45 +0530

    Update backend.tf

--- live/backend.tf
+++ live/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraform-demo-ccprasad"
+    key            = "env/dev/terraform.tfstate"
+    region         = "us-east-1"                  # <- specify your region
+    dynamodb_table = "terraform-lock-table"
+  }
 }
