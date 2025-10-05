55ece79ed9fc299c984bd092c1f3338f452a4ca1
prasad0108-ux <suryavanshipr30@gmail.com>
2025-10-05 10:45:17 +0530

    Update main.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -4,18 +4,24 @@ provider "aws" {
 
 resource "aws_s3_bucket" "tf_state" {
   bucket = var.state_bucket_name
-  tags = { Name = "terraform-state" }
+  tags = { 
+    Name = "terraform-state" 
+  }
 }
 
 resource "aws_s3_bucket_versioning" "tf_state_versioning" {
   bucket = aws_s3_bucket.tf_state.id
-  versioning_configuration { status = "Enabled" }
+  versioning_configuration { 
+    status = "Enabled" 
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_enc" {
   bucket = aws_s3_bucket.tf_state.id
   rule {
-    apply_server_side_encryption_by_default { sse_algorithm = "AES256" }
+    apply_server_side_encryption_by_default { 
+      sse_algorithm = "AES256" 
+    }
   }
 }
 
@@ -31,5 +37,9 @@ resource "aws_dynamodb_table" "tf_locks" {
   name         = "terraform-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-  attribute { name = "LockID"  type = "S" }
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
